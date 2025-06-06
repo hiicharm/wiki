ca6b5738fd3472806e498552de1e8b0793d792c8
roirepus <rajsinsinwar159@gmail.com>
2025-06-06 22:01:27 +0530

    init

--- .source/index.ts
+++ .source/index.ts
@@ -1,7 +1,6 @@
 // @ts-nocheck -- skip type checking
-import * as docs_2 from "../content/docs/devops/setup-vps-for-production.mdx?collection=docs&hash=1749163124166"
-import * as docs_1 from "../content/docs/devops/index.mdx?collection=docs&hash=1749163124166"
+import * as docs_1 from "../content/docs/devops/setup-vps-for-production.mdx?collection=docs&hash=1749163124166"
 import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1749163124166"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/home/charm/hiicharm/wiki/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"devops/index.mdx","absolutePath":"/home/charm/hiicharm/wiki/content/docs/devops/index.mdx"}, data: docs_1 }, { info: {"path":"devops/setup-vps-for-production.mdx","absolutePath":"/home/charm/hiicharm/wiki/content/docs/devops/setup-vps-for-production.mdx"}, data: docs_2 }], [{"info":{"path":"devops/meta.json","absolutePath":"/home/charm/hiicharm/wiki/content/docs/devops/meta.json"},"data":{"title":"DevOps","pages":["setup-vps-for-production"],"defaultOpen":true,"icon":"MyIcon"}}])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/home/charm/hiicharm/wiki/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"devops/setup-vps-for-production.mdx","absolutePath":"/home/charm/hiicharm/wiki/content/docs/devops/setup-vps-for-production.mdx"}, data: docs_1 }], [{"info":{"path":"devops/meta.json","absolutePath":"/home/charm/hiicharm/wiki/content/docs/devops/meta.json"},"data":{"title":"DevOps","pages":["setup-vps-for-production"],"defaultOpen":true,"icon":"MyIcon"}}])
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Link from 'next/link';
 
 /**
  * Shared layout configurations
