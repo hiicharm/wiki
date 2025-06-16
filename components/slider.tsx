4f1c510e8c36f87867fac48539148556d7e90a56
roirepus <rajsinsinwar159@gmail.com>
2025-06-17 02:41:53 +0530

    rust ownership

---
+++ components/slider.tsx
@@ -0,0 +1,63 @@
+"use client"
+import React, { useRef, useState } from 'react';
+
+interface ImageSliderProps {
+  images: string[]; // Array of image URLs
+}
+
+const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [scale, setScale] = useState(100);
+
+  const scroll = (direction: 'left' | 'right') => {
+    if (!containerRef.current) return;
+
+    const { scrollLeft, clientWidth } = containerRef.current;
+    const scrollAmount = clientWidth * 0.9;
+
+    containerRef.current.scrollTo({
+      left: direction === 'left' ? scrollLeft - scrollAmount : scrollLeft + scrollAmount,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div
+      className={`img-cont relative w-full
+`}>
+      {/* Arrows */}
+      <button
+        onClick={() => scroll('left')}
+        className="absolute left-0 top-1/2 -translate-y-1/2 z-10 bg-white/70 hover:bg-white rounded-full p-2 shadow"
+      >
+        ◀
+      </button>
+
+      <div
+        ref={containerRef}
+        className="flex overflow-x-auto gap-4 scroll-smooth scrollbar-hide py-2 px-10"
+      >
+        {images.map((src, index) => (
+
+          <img
+            key={index}
+            src={src}
+            alt={`Image ${index + 1}`}
+            className={`flex-shrink-0 rounded-lg shadow-md
+  scale-${scale}`}
+          />
+        ))
+        }
+      </div >
+
+      <button
+        onClick={() => scroll('right')}
+        className="absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-white/70 hover:bg-white rounded-full p-2 shadow"
+      >
+        ▶
+      </button>
+    </div >
+  );
+};
+
+export default ImageSlider;
---
+++ constants/rust/index.ts
@@ -0,0 +1,7 @@
+
+export const images = [
+  "/rust/ownership/ownership1.png",
+  "/rust/ownership/ownership2.png",
+  "/rust/ownership/ownership3.png",
+  "/rust/ownership/ownership4.png",
+]
