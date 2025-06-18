538f76ef6ea8ae73a5ceb69d1c22f5d45adbb2ee
roirepus <rajsinsinwar159@gmail.com>
2025-06-18 23:06:13 +0530

    enums and structs

--- constants/rust/index.ts
+++ constants/rust/index.ts
@@ -1,16 +1,35 @@
 
 export const ownershipImages = [
-  "/rust/ownership/ownership1.png",
-  "/rust/ownership/ownership2.png",
-  "/rust/ownership/ownership3.png",
-  "/rust/ownership/ownership4.png",
+  "/rust/ownership/ownership1.jpg",
+  "/rust/ownership/ownership2.jpg",
+  "/rust/ownership/ownership3.jpg",
+  "/rust/ownership/ownership4.jpg",
 ]
 
 export const borrowingImages = [
-  "/rust/borrowing/borrowing1.png",
-  "/rust/borrowing/borrowing2.png",
-  "/rust/borrowing/borrowing3.png",
-  "/rust/borrowing/borrowing4.png",
-  "/rust/borrowing/borrowing5.png",
-  "/rust/borrowing/borrowing6.png",
+  "/rust/borrowing/borrowing1.jpg",
+  "/rust/borrowing/borrowing2.jpg",
+  "/rust/borrowing/borrowing3.jpg",
+  "/rust/borrowing/borrowing4.jpg",
+  "/rust/borrowing/borrowing5.jpg",
+  "/rust/borrowing/borrowing6.jpg",
+]
+
+export const structsImages = [
+  "/rust/structs/structs1.jpg",
+  "/rust/structs/structs2.jpg",
+  "/rust/structs/method_syntax1.jpg",
+  "/rust/structs/method_syntax2.jpg",
+]
+
+export const enumsImages = [
+  "/rust/enums/enums1.jpg",
+  "/rust/enums/enums2.jpg",
+  "/rust/enums/enums3.jpg",
+]
+export const patternMatchingImages = [
+
+  "/rust/pattern_matching/pattern_matching1.jpg",
+  "/rust/pattern_matching/pattern_matching2.jpg",
+  "/rust/pattern_matching/pattern_matching3.jpg",
 ]
---
+++ content/docs/rust/index.md
@@ -0,0 +1,9 @@
+---
+title: Intro
+description: Intro to Rust
+---
+This is a collection of resources I'm following and encountering while learning rust.  
+Primary source is [the book](https://doc.rust-lang.org/book/)   
+Supplementary (with some basics missing, but some topics explained quite well) [Brown University Version](https://rust-book.cs.brown.edu/)   
+For now, I'm just uploading my handwritten notes directly (colorful notes, brain go happi happi). I might add other resources and optimise stuff later.
+
